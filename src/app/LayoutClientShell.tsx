e5d27d601a3ed8c502f28458599d8798688febed
MinhLuong30 <minhltse182480@fpt.edu.vn>
2025-06-06 04:39:43 +0700

    fix page.tsx

--- src/app/LayoutClientShell.tsx
+++ src/app/LayoutClientShell.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import "@ant-design/v5-patch-for-react-19";
 import React from 'react';
 import { usePathname } from "next/navigation";
 import Sidebar from "@/components/Sidebar/Sidebar";
